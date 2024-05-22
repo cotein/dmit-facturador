12095492f2c15f8329e42696ef21a57e4354385b
Diego Barrueta <diego.barrueta@gmail.com>
2024-05-22 16:56:57 -0300

    build options

--- vite.config.ts
+++ vite.config.ts
@@ -31,44 +31,44 @@ export default defineConfig({
 		copy({
 			targets: [
 				{
-					src: `/src/assets/vendor/bootstrap/css/bootstrap.min.css`,
-					dest: 'dist/vendor/bootstrap/css/bootstrap.min.css`',
+					src: 'src/assets/vendor/bootstrap/css/bootstrap.min.css',
+					dest: 'dist/assets/vendor/bootstrap/css',
 				},
 				{
-					src: `/src/assets/vendor/bootstrap-icons/bootstrap-icons.css`,
-					dest: 'dist/vendor/bootstrap-icons/bootstrap-icons.css`',
+					src: 'src/assets/vendor/bootstrap-icons/bootstrap-icons.css',
+					dest: 'dist/assets/vendor/bootstrap-icons',
 				},
 				{
-					src: `/src/assets/vendor/boxicons/css/boxicons.min.css`,
-					dest: 'dist/vendor/boxicons/css/boxicons.min.css`',
+					src: 'src/assets/vendor/boxicons/css/boxicons.min.css',
+					dest: 'dist/assets/vendor/boxicons/css',
 				},
-				{ src: `/src/assets/vendor/remixicon/remixicon.css`, dest: 'dist/vendor/remixicon/remixicon.css`' },
+				{ src: 'src/assets/vendor/remixicon/remixicon.css', dest: 'dist/assets/vendor/remixicon' },
 				{
-					src: `/src/assets/vendor/swiper/swiper-bundle.min.css`,
-					dest: 'dist/vendor/swiper/swiper-bundle.min.css`',
+					src: 'src/assets/vendor/swiper/swiper-bundle.min.css',
+					dest: 'dist/assets/vendor/swiper',
 				},
-				{ src: `/src/assets/vendor/css/style.css`, dest: 'dist/vendor/css/style.css`' },
+				{ src: 'src/assets/vendor/css/style.css', dest: 'dist/assets/css' },
 				{
-					src: `/src/assets/vendor/bootstrap/js/bootstrap.bundle.min.js`,
-					dest: 'dist/vendor/bootstrap/js/bootstrap.bundle.min.js`',
-				},
-				{
-					src: `/src/assets/vendor/isotope-layout/isotope.pkgd.min.js`,
-					dest: 'dist/vendor/bootstrap/js/bootstrap.bundle.min.js`',
+					src: 'src/assets/vendor/bootstrap/js/bootstrap.bundle.min.js',
+					dest: 'dist/assets/vendor/bootstrap/js',
 				},
 				{
-					src: `/src/assets/vendor/php-email-form/validate.js`,
-					dest: 'dist/vendor/bootstrap/js/bootstrap.bundle.min.js`',
+					src: 'src/assets/vendor/isotope-layout/isotope.pkgd.min.js',
+					dest: 'dist/assets/vendor/isotope-layout',
 				},
 				{
-					src: `/src/assets/vendor/swiper/swiper-bundle.min.js`,
-					dest: 'dist/vendor/bootstrap/js/bootstrap.bundle.min.js`',
+					src: 'src/assets/vendor/php-email-form/validate.js',
+					dest: 'dist/assets/vendor/php-email-form',
 				},
 				{
-					src: `/src/assets/vendor/waypoints/noframework.waypoints.js`,
-					dest: 'dist/vendor/bootstrap/js/bootstrap.bundle.min.js`',
+					src: 'src/assets/vendor/swiper/swiper-bundle.min.js',
+					dest: 'dist/assets/vendor/swiper',
 				},
-				{ src: `/src/assets/vendor/js/main.js`, dest: 'dist/vendor/bootstrap/js/bootstrap.bundle.min.js`' },
+				{
+					src: 'src/assets/vendor/waypoints/noframework.waypoints.js',
+					dest: 'dist/assets/vendor/waypoints',
+				},
+				{ src: 'src/assets/vendor/js/main.js', dest: 'dist/assets/js' },
 			],
 			hook: 'writeBundle', // ensure the files are copied before the bundle is written
 		}),
