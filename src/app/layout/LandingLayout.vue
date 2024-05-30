f9b7327769b679d6b19a9ff4a47ffab0a7f15805
Diego Barrueta <diego.barrueta@gmail.com>
2024-05-30 13:52:08 -0300

    vite.config.js

--- src/app/layout/LandingLayout.vue
+++ src/app/layout/LandingLayout.vue
@@ -2,9 +2,9 @@
 	<a-layout>
 		<header id="header" class="fixed-top">
 			<div class="container d-flex align-items-center">
-				<h1 class="logo me-auto"><a href="index.html">DMIT</a><small> Facturador en línea</small></h1>
+				<h1 class="logo me-auto"><a href="www.dmit.ar">DMIT</a><small> Facturador en línea</small></h1>
 				<!-- Uncomment below if you prefer to use an image logo -->
-				<!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+				<!-- <a href="www.dmit.ar" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
 				<nav id="navbar" class="navbar">
 					<ul>
--- vite.config.ts
+++ vite.config.ts
@@ -30,59 +30,7 @@ export default defineConfig({
 		}),
 		copy({
 			targets: [{ src: 'src/assets/*', dest: 'dist/assets' }],
-			/* targets: [
-				{
-					src: 'src/assets/vendor/bootstrap/css/bootstrap.min.css',
-					dest: 'dist/assets/vendor/bootstrap/css',
-				},
-				{
-					src: 'src/assets/vendor/bootstrap-icons/bootstrap-icons.css',
-					dest: 'dist/assets/vendor/bootstrap-icons',
-				},
-				{
-					src: 'src/assets/vendor/bootstrap-icons/fonts/bootstrap-icons.woff',
-					dest: 'dist/assets/vendor/bootstrap-icons/fonts',
-				},
-				{
-					src: 'src/assets/vendor/bootstrap-icons/fonts/bootstrap-icons.woff2',
-					dest: 'dist/assets/vendor/bootstrap-icons/fonts',
-				},
-				{
-					src: 'src/assets/vendor/boxicons/css/boxicons.min.css',
-					dest: 'dist/assets/vendor/boxicons/css',
-				},
-				{
-					src: 'src/assets/vendor/boxicons/css/boxicons.min.css',
-					dest: 'dist/assets/vendor/boxicons/css',
-				},
-				{ src: 'src/assets/vendor/remixicon/remixicon.css', dest: 'dist/assets/vendor/remixicon' },
-				{
-					src: 'src/assets/vendor/swiper/swiper-bundle.min.css',
-					dest: 'dist/assets/vendor/swiper',
-				},
-				{ src: 'src/assets/vendor/css/style.css', dest: 'dist/assets/css' },
-				{
-					src: 'src/assets/vendor/bootstrap/js/bootstrap.bundle.min.js',
-					dest: 'dist/assets/vendor/bootstrap/js',
-				},
-				{
-					src: 'src/assets/vendor/isotope-layout/isotope.pkgd.min.js',
-					dest: 'dist/assets/vendor/isotope-layout',
-				},
-				{
-					src: 'src/assets/vendor/php-email-form/validate.js',
-					dest: 'dist/assets/vendor/php-email-form',
-				},
-				{
-					src: 'src/assets/vendor/swiper/swiper-bundle.min.js',
-					dest: 'dist/assets/vendor/swiper',
-				},
-				{
-					src: 'src/assets/vendor/waypoints/noframework.waypoints.js',
-					dest: 'dist/assets/vendor/waypoints',
-				},
-				{ src: 'src/assets/vendor/js/main.js', dest: 'dist/assets/js' },
-			], */
+
 			hook: 'writeBundle', // ensure the files are copied before the bundle is written
 		}),
 	],
