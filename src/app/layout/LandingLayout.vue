82d8b6ee9fd0e7533dabbcac91cef8b8f5db015f
Diego Barrueta <diego.barrueta@gmail.com>
2024-06-07 16:49:00 -0300

    improve load site

--- src/app/layout/LandingLayout.vue
+++ src/app/layout/LandingLayout.vue
@@ -2,7 +2,7 @@
     <a-layout>
         <div id="preloader" v-if="isLoading"></div>
         <header id="header" class="fixed-top">
-            <div class="container d-flex align-items-center">
+            <div class="container d-flex align-items-center" v-if="!isLoading">
                 <h1 class="logo me-auto"><a href="www.dmit.ar">DMIT</a><small> Facturador en línea</small></h1>
                 <!-- Uncomment below if you prefer to use an image logo -->
                 <!-- <a href="www.dmit.ar" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
