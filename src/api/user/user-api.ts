9b9beb4d9590d33451d04dc016a55328e1d0a934
Diego Barrueta <diego.barrueta@gmail.com>
2024-12-30 11:38:09 -0300

    pinia problem

--- src/api/user/user-api.ts
+++ src/api/user/user-api.ts
@@ -1,9 +1,13 @@
 import type { LoggedUser } from '@/app/types/User';
 import { ApiHttp } from '../base-api';
 import type { AxiosResponse } from 'axios';
+import { useSleepComposable } from '@/app/composables/sleep/useSleepComposable';
+
+const { sleep } = useSleepComposable();
 
 export const getMyData = async (): Promise<any> => {
     try {
+        await sleep(1500);
         const response = await ApiHttp.get<AxiosResponse<LoggedUser>>('/api/users');
 
         return response;
